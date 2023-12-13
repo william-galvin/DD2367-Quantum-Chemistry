5716209b8e4e2e0e57d2bd26368d06b8ae95f674
William Galvin <williamgalvin246@gmail.com>
2023-12-13 16:16:36 +0100

    initial commit

---
+++ givens.py
@@ -0,0 +1,188 @@
+import numpy as np
+
+from typing import Dict
+from typing import Tuple
+from typing import List
+
+def givens(electrons: int, qubits: int, thetas: List[float]) -> np.ndarray:
+    """
+    Finds the full transition matrix for a given system and given thetas
+
+    example:
+    ```python
+
+    thetas = [0, 0, .2]
+    electrons, qubits = 2, 4
+
+    G = givens(electrons, qubits, thetas)
+    psi_valid = hf_ground(electrons, qubits) @ G
+
+    H = hamiltonian.hamiltonian(...)
+    psi = full_wavefunction(psi_valid, qubits)
+
+    E = hamiltonian.expectation(H, psi)
+
+    ```
+    """
+    states = get_states(electrons, qubits)
+    singles, doubles = excitations(electrons, qubits)
+
+    M = np.eye(len(states))
+
+    for i, (src, dst) in enumerate(singles):
+        M = M @ givens_single(src, dst, thetas[i], states)
+
+    for i, idxs in enumerate(doubles):
+        src = idxs[: len(idxs) // 2]
+        dst = idxs[len(idxs) // 2 :]
+        M = M @ givens_double(src, dst, thetas[i + len(singles)], states)
+
+    return M
+
+
+def hf_ground(electrons: int, qubits: int) -> np.array:
+    """ 
+    Returns the hf-ground state of the wave function. The wave function vector
+    contains only states thar are chemically valid (correct number of electrons)
+    """
+    states = get_states(electrons, qubits)
+
+    hf_ground = np.zeros(len(states))
+    hf_ground_str = "1" * electrons + "0" * (qubits - electrons)
+    hf_ground[states[hf_ground_str][0]] = 1
+
+    return hf_ground
+
+
+def full_wavefunction(valid_wavefunction: np.array, qubits: int) -> np.array:
+    """
+    Converts a wave function containing only the valid states to the full wave function
+    """
+    states = get_states(electrons, qubits)
+    full_psi = np.zeros(2 ** qubits)
+    for state, (valid_idx, full_idx) in states.items():
+        full_psi[full_idx] = valid_wavefunction[valid_idx]
+    return full_psi
+
+
+def excitations(electrons: int, orbitals: int) -> Tuple[List[int], List[int]]:
+    """
+    Enumerate the valid excitations.
+    Exact copy of PennyLane implementation.
+    https://docs.pennylane.ai/en/stable/_modules/pennylane/qchem/structure.html#excitations
+    """
+    delta_sz = 0
+    sz = np.array([0.5 if (i % 2 == 0) else -0.5 for i in range(orbitals)])
+
+    singles = [
+        [r, p]
+        for r in range(electrons)
+        for p in range(electrons, orbitals)
+        if sz[p] - sz[r] == delta_sz
+    ]
+
+    doubles = [
+        [s, r, q, p]
+        for s in range(electrons - 1)
+        for r in range(s + 1, electrons)
+        for q in range(electrons, orbitals - 1)
+        for p in range(q + 1, orbitals)
+        if (sz[p] + sz[q] - sz[r] - sz[s]) == delta_sz
+    ]
+
+    return singles, doubles
+
+
+def get_states(electrons: int, qubits: int) -> Dict[str, Tuple[int, int]]:
+    """
+    Finds the valid hf states for the given
+    qubits (spin orbitals) and electrons.
+
+    This is much less than the full 2 ** qubits states, so
+    return is dictionary of binary strings of length q to 
+    (valid_idx, i) where valid_idx is n when the state is the nth valid
+    state seen, and i is the index into the full 2 ** qubits states
+    """
+    states = {}
+    valid_idx = 0
+    for i in range(2 ** qubits):
+        if bin(i).count("1") == electrons:
+            state_str = format(i, f"#0{qubits + 2}b")[2:]
+            states[state_str] = (valid_idx, i)
+            valid_idx += 1
+
+    return states
+
+
+def get_flipped_state(state: str, src: int, dst: int) -> str:
+    """
+    Given a binary string, flips the bits at src and dst
+    """
+    res = ""
+    for i, c in enumerate(state):
+        if i == src or i == dst:
+            res += str(1 - int(c))
+        else:
+            res += c
+    return res
+
+
+def givens_single(src: int, dst: int, theta: float, states: Dict[str, Tuple[int, int]]) -> np.ndarray:
+    """
+    Returns the state transition matrix for a single excitation
+    parameterized by theta.
+
+    Src, dst should be ints
+    theta should be a scalar.
+
+    Returned matrix will not be  2 ** q x 2 ** q, but rather 
+    len(states) x len(states)
+    """
+    M = np.eye(len(states))
+    for state, (i, _) in states.items():
+        if state[src] == "1" and state[dst] == "0":
+            flipped = get_flipped_state(state, src, dst)
+            j, _ = states[flipped]
+
+            M[i, i] =  np.cos(theta / 2)  
+            M[i, j] = -np.sin(theta / 2)
+            M[j, i] =  np.sin(theta / 2)
+            M[j, j] =  np.cos(theta / 2)
+    return M
+
+
+def givens_double(src: List[int], dst: List[int], theta: float, states: Dict[str, Tuple[int, int]]) -> np.ndarray:
+    """
+    Returns the state transition matrix for a double excitation
+    parameterized by theta.
+
+    Src, dst should be lists of ints of size 2
+    theta should be a scalar.
+
+    Returned matrix will not be  2 ** q x 2 ** q, but rather 
+    len(states) x len(states)
+    """
+    M = np.eye(len(states))
+    for state, (i, _) in states.items():
+        if state[src[0]] == "1" and state[dst[0]] == "0" and state[src[1]] == "1" and state[dst[1]] == "0":
+            flipped = get_flipped_state(state, src[0], dst[0])
+            flipped = get_flipped_state(flipped, src[1], dst[1])
+            j, _ = states[flipped]
+
+            M[i, i] =  np.cos(theta / 2)
+            M[i, j] = -np.sin(theta / 2)
+            M[j, i] =  np.sin(theta / 2)
+            M[j, j] =  np.cos(theta / 2)
+    return M
+
+
+if __name__ == '__main__':
+    electrons, qubits = 2, 4
+    thetas = [0, 0, .2]
+    electrons, qubits = 2, 4
+
+    G = givens(electrons, qubits, thetas)
+    psi_valid = hf_ground(electrons, qubits) @ G
+    psi = full_wavefunction(psi_valid, qubits)
+
+    print(psi)
---
+++ hamiltonian.py
@@ -0,0 +1,158 @@
+import numpy as np
+import pyscf
+import itertools
+import openfermion
+import openfermionpyscf
+
+from tqdm import tqdm
+
+
+X = np.array([[0,  1 ], [1,  0]], dtype=complex)
+Y = np.array([[0, -1j], [1j, 0]], dtype=complex)
+Z = np.array([[1,  0 ], [0, -1]], dtype=complex)
+I = np.array([[1,  0 ], [0,  1]], dtype=complex)
+
+
+def hamiltonian(xyz_path: str, basis: str = "sto-3g") -> np.ndarray:
+    """
+    Constructs the electronic hamiltonian for a given molecule
+
+    Params:
+    - xyz_path: path to .xyz file containing coordinates of atoms. These
+      can be found here: https://cccbdb.nist.gov/expgeom1x.asp among other places.
+      (Enter your formula in the text box, scroll down until you see each atom with a 
+      list of x,y,z coordinates).
+
+    - basis: molecurlar orbital basis to use. Defaults to 'sto-3g'
+
+    Returns array/gate representation of the electronic hamiltonian
+    """
+    mol_pyscf = pyscf.gto.M(atom=xyz_path, basis=basis)
+
+    # mol = pyscf_to_openfermion(mol_pyscf)
+    # openfermionpyscf.run_pyscf(mol)
+    # one_e, two_e = mol.get_integrals()
+
+    hf = mol_pyscf.RHF()
+    hf.kernel()
+    for j in range(10):
+        mo = hf.stability()[0]
+        if np.allclose(mo, hf.mo_coeff):
+            break
+        dm = hf.make_rdm1(mo, hf.mo_occ)
+        hf = hf.run(dm)
+
+    h1e = mol_pyscf.intor("int1e_kin") + mol_pyscf.intor("int1e_nuc")
+    h2e = mol_pyscf.intor("int2e")
+
+    scf_c = hf.mo_coeff
+    nuclear_repulsion = mol_pyscf.energy_nuc()
+    constant = nuclear_repulsion
+
+    one_e = scf_c.T @ h1e @ scf_c
+    for i in range(4):
+        h2e = np.tensordot(h2e, scf_c, axes=1).transpose(3, 0, 1, 2)
+    two_e = h2e.transpose(0, 2, 3, 1)
+
+    N = len(mol_pyscf.spinor_labels())
+
+    H = np.zeros((2 ** N, 2 ** N), dtype=complex)
+
+    for p, q in tqdm(itertools.product(range(N), range(N))):
+        if p % 2 == q % 2:
+            H += one_e[p // 2, q // 2] * creation(p, N) @ annihilation(q, N)
+
+    for p, q, r, s in tqdm(itertools.product(range(N), range(N), range(N), range(N))):
+        if p % 2 == r % 2 and q % 2 == s % 2:
+            H += 0.5 * two_e[p // 2, q // 2, r // 2, s // 2] * creation(p, N) @ creation(q, N) @ annihilation(r, N) @ annihilation(s, N)
+
+    return H  + constant
+
+
+def pyscf_to_openfermion(mol: pyscf.gto.mole.Mole) -> openfermion.chem.MolecularData:
+    geometry = []
+    for line in mol.tostring().split("\n"):
+        sym, x, y, z = line.split()
+        x, y, z = float(x), float(y), float(z)
+        geometry.append((sym, (x, y, z)))
+
+    return openfermion.chem.MolecularData(
+        geometry=geometry,
+        basis=mol.basis,
+        multiplicity=mol.multiplicity
+    )
+
+
+def creation(i: int, N: int) -> np.ndarray:
+    """
+    Returns a 2^N x 2^N matrix representing the creation_i operator
+
+    Params:
+    - i: (0-indexed) spin orbital to create 
+    - N: number of spin orbitals
+
+    Example:
+    ```python
+    (creation(1, 2)
+    >>>
+    [[ 0.+0.j  0.+0.j  0.+0.j  0.+0.j]
+    [ 1.+0.j  0.+0.j  0.+0.j  0.+0.j]
+    [ 0.+0.j  0.+0.j -0.+0.j -0.+0.j]
+    [ 0.+0.j  0.+0.j -1.+0.j -0.+0.j]]
+    ```
+    """
+    A = np.array([[1]])
+    for n in range(N):
+        if n < i:
+            t = Z
+        elif n == i:
+            t = (X - 1j * Y) / 2
+        else:
+            t = I
+        A = np.kron(A, t)
+    return A
+
+
+def annihilation(i: int, N: int) -> np.ndarray:
+    """
+    Returns a 2^N x 2^N matrix representing the annihilation_i operator
+
+    Params:
+    - i: (0-indexed) spin orbital to create
+    - N: number of spin orbitals
+
+    Example:
+    ```python
+    annihilation(1, 2)
+    >>>
+    [[ 0.+0.j  1.+0.j  0.+0.j  0.+0.j]
+    [ 0.+0.j  0.+0.j  0.+0.j  0.+0.j]
+    [ 0.+0.j  0.+0.j -0.+0.j -1.+0.j]
+    [ 0.+0.j  0.+0.j -0.+0.j -0.+0.j]]
+    ```
+    """
+    A =np.array([[1]])
+    for n in range(N):
+        if n < i:
+            t = Z
+        elif n == i:
+            t = (X + 1j * Y) / 2
+        else:
+            t = I
+        A = np.kron(A, t)
+    return A
+
+
+def expectation(H: np.ndarray, psi: np.array) -> float:
+    """
+    Calculates <psi*|H|psi>/<psi*|psi> = the expectation
+    of the Hamiltonian with respect to the wave function
+    """
+    return (psi.conj() @ H @ psi) / (psi.conj() @ psi)
+
+
+if __name__ == "__main__":
+    H = hamiltonian("data/h2.xyz")
+    print(H.shape)
+    np.save("h2_ham.npy", H)
+
